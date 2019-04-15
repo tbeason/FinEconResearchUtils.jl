48bf4ff5cb36f3e147b4ad4c08eeabee16fa6dfd
Tyler Beason <tbeas12@gmail.com>
2019-04-15 12:30:40 -0700

    add rollingfillfirst and others

--- src/utils.jl
+++ src/utils.jl
@@ -1,5 +1,7 @@
 using LinearAlgebra
+using RollingFunctions
 import SparseArrays: spdiagm
+
 
 # remove linear time trend
 function detrend(y::Vector{Float64})
@@ -30,5 +32,56 @@ function hp_filter(y::Vector{Float64}, lambda::Real;detrendfirst::Bool = false)
 	t, y-t
 end
 
+lastnonzero(X) = (y=filter(!iszero,X); !isempty(y) ? last(y) : 0.0)
+dropcols(df::AbstractDataFrame, cols) = df[setdiff(names(df), cols)]
+yrqtrfun(dt) = string(year(dt),"Q",quarterofyear(dt))
+
+
+
+function rollingfillfirst(fun::Function, data::AbstractArray{T1,1}, windowspan::Int64,filler::T2) where {T1,T2<:Union{Missing,Number}}
+	TL = size(data,1)
+	FL = windowspan - 1
+	outtmp = allowmissing(zeros(TL))
+	outtmp[1:FL] .= filler
+	outtmp[(1+FL):end]=rolling(fun,data,windowspan)
+	return outtmp
+end
+
+
+
+
+
+
+
+function tomissing!(d::AbstractDataFrame,x::Union{Integer,Symbol})
+	allowmissing!(d,x)
+	d[findall(isnan,d[x]),x] = missing
+end
+
+function alltomissing!(d::AbstractDataFrame)
+	for nm in names(d)
+		if eltype(d[nm]) <: Float64
+			if any(isnan,d[nm])
+				tomissing!(d,nm)
+			end
+		end
+	end
+end
+
+
+
+function maxdrawdown(R)
+	T = length(R)
+	P = 100 .* cumprod(1 .+ R)
+	DD = zeros(size(R))
+	peak = -999.99
+	for i = 1:T
+		if P[i] > peak
+			peak = P[i]
+		end
+		DD[i] = (P[i]-peak)/peak
+	end
+	maxDD = minimum(DD)
+end
 
 
